ebfec9dea466fd70a8788fea0f88c61fd78b21ff
Paulo Martins <pmartins@duck.com>
2024-09-01 18:45:51 -0300

    criacao da funcao feat_eng

--- PROJETOS/db-pipeline/app.py
+++ PROJETOS/db-pipeline/app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import assets.utils as utils
 from assets.utils import logger
 import datetime
+import numpy as np
 
 load_dotenv()
 
@@ -30,14 +31,41 @@ def data_clean(df, metadados):
     logger.info(f'Saneamento concluído; {datetime.datetime.now()}')
     return df
 
+def classifica_hora(hra):
+    if 0 <= hra < 6: return "MADRUGADA"
+    elif 6 <= hra < 12: return "MANHA"
+    elif 12 <= hra < 18: return "TARDE"
+    else: return "NOITE"
+
 def feat_eng(df):
     '''
-    Função ???????????????????????????
-    INPUT: ???????????????????????????
-    OUTPUT: ???????????????????????????
+    Função para criação de novas colunas para análise (Feature Engineering)
+    INPUT: Pandas DataFrame
+    OUTPUT: Pandas DataFrame com novas colunas: tempo_voo_esperado, tempo_voo_hr, atraso , dia_semana, flag_atraso, flag_adiantado e horario (manha/tarde/noite/madrugada)
     '''
-    #colocar log info
-    pass
+    df["tempo_voo_esperado"] = (df["datetime_chegada_formatted"] - df["datetime_partida_formatted"]) / pd.Timedelta(hours=1)
+    logger.info(f'Coluna tempo_voo_esperado criada. {datetime.datetime.now()}')
+    
+    df["tempo_voo_hr"] = df["tempo_voo"] /60
+    logger.info(f'Coluna tempo_voo_hr criada. {datetime.datetime.now()}')
+    
+    df["atraso"] = df["tempo_voo_hr"] - df["tempo_voo_esperado"]
+    logger.info(f'Coluna atraso criada. {datetime.datetime.now()}')
+    
+    df["flag_atraso"] = np.where(df['atraso'] > 0.6, True, False)
+    logger.info(f'Coluna flag_atraso criada. {datetime.datetime.now()}')
+    
+    df["flag_adiantado"] = np.where(df['atraso'] < -0.5, True, False)
+    logger.info(f'Coluna flag_adiantado criada. {datetime.datetime.now()}')
+    
+    df["dia_semana"] = df["data_voo"].dt.day_of_week #0=segunda
+    logger.info(f'Coluna dia_semana criada. {datetime.datetime.now()}')
+
+    df["horario"] = df.loc[:,"datetime_partida_formatted"].dt.hour.apply(lambda x: classifica_hora(x))
+    logger.info(f'Coluna horario criada. {datetime.datetime.now()}')
+
+    logger.info(f'Featuring Engineering concluida: {datetime.datetime.now()}')
+    return df
 
 def save_data_sqlite(df):
     try:
@@ -73,6 +101,6 @@ if __name__ == "__main__":
     utils.null_check(df, metadados["null_tolerance"])
     utils.keys_check(df, metadados["cols_chaves"])
     df = feat_eng(df)
-    #save_data_sqlite(df)
+    save_data_sqlite(df)
     fetch_sqlite_data(metadados["tabela"][0])
     logger.info(f'Fim da execução ; {datetime.datetime.now()}')
